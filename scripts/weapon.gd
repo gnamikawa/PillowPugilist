6f8f831410031688fc109e0ce8f5e17a5822f454
Calandiel <Calandiel@users.noreply.github.com>
2023-08-12 15:02:08 +0900

    POPPIN OFF

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -4,6 +4,8 @@ extends Node
 
 @export var shots_per_minute: float
 @export var bullet_scene: PackedScene
+@export var clip: AudioStream
+
 var timer = 0
 const MIN_SHOTS_PER_MINUTE = 1 # Just to avoid division by 0...
 
@@ -18,7 +20,16 @@ func shoot(delta_time: float, bullet_parent, direction = Vector2(0, 1), position
 			bullet.position = position
 		bullet.player_spawned = player_spawned
 		bullet.direction = direction
-		print('spawning : ', bullet.spin_speed)
 		# Adds the bullet to the scene tree
 		# (necessary as otherwise it won't render!)
 		bullet_parent.add_child(bullet)
+		
+		if get_child_count() > 0:
+			var player = get_child(0) as AudioStreamPlayer2D
+			if player:
+				if player.stream != clip:
+					player.stream = clip
+				player.play()
+		
+		return true
+	return false
