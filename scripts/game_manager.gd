13bc825cb77de84448b7ff1b42168b8c66dabf0a
Genzo <genzo.namikawa@outlook.com>
2023-08-13 15:04:59 +0900

    add music and music loop for level 1

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,10 +2,5 @@ class_name GameManager
 
 extends Node2D
 
-@export var soundTrack: AudioStreamWAV;
-
-@onready var musicManager: MusicManager = $MusicManager;
-
 func _ready():
 	print('-- game manager ready --')
-	musicManager.crossfade_to(soundTrack)
--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -6,34 +6,48 @@ extends Node
 @onready var _trackA := $MusicPlayerA
 @onready var _trackB := $MusicPlayerB
 
+@export var initialTrack: AudioStream
+@export var transitionScalar: float = 1
+
 # If a queue fails, attach a cb to try again.
 signal onTransitionFinished;
 
+func _ready():
+	print("-- music manager ready --")
+	onTransitionFinished.connect(_debugOnTransitionFinished)
+	crossfade_to(initialTrack, 9999) # Dont crossfade
+
+func _debugOnTransitionFinished():
+	print("-- Transition completed --")
+	if _trackA.playing:
+		print("Track A playing")
+	if _trackB.playing:
+		print("Track B playing")
+
 # Crossfades to a new audio stream 
 # Returns false on queue failure
-func crossfade_to(audio_stream: AudioStream) -> bool:
+func crossfade_to(audio_stream: AudioStream, speed_scale: float = 1) -> void:
 	# If both tracks are playing, we're calling the function in the middle of a fade.
-	# We return early to avoid jumps in the sound.
+	# We queue the transition instead.
 	if _trackA.playing and _trackB.playing:
-		return false
+		print("transition");
+		return
 
 	# The `playing` property of the stream players tells us which track is active. 
 	# If it's track two, we fade to track one, and vice-versa.
 	if _trackB.playing:
 		_trackA.stream = audio_stream
 		_trackA.play()
+		_anim_player.speed_scale = speed_scale
 		_anim_player.play("FadeToTrackA")
 		onTransitionFinished.emit()
-		return true
 	else:
 		_trackB.stream = audio_stream
 		_trackB.play()
+		_anim_player.speed_scale = speed_scale
 		_anim_player.play("FadeToTrackB")
 		onTransitionFinished.emit()
-		return true
 
-func crossfade_out():
+func crossfade_out(speed_scale: float):
 	var empty_audio_stream = AudioStreamWAV.new()
-	var isCrossfadeQueued = crossfade_to(empty_audio_stream)
-	if(!isCrossfadeQueued):
-		onTransitionFinished.connect(crossfade_out)
+	crossfade_to(empty_audio_stream, speed_scale)
